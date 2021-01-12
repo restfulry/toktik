0ff5be904872fc2764e0a872f74afa6dfffb0f68
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-12 18:51:03 -0500

    overload __str__self for Answer

--- main_app/models.py
+++ main_app/models.py
@@ -42,3 +42,6 @@ class Answer(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.answer
