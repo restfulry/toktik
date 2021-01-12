486229fc7faf6a7e55d438eda805cc5df4825745
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-11 22:06:01 -0500

    update Question model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+CATEGORIES = (
+    ('SCI', 'SCIENCE'),
+    ('MAT', 'MATHEMATICS'),
+    ('GAR', 'GARDEN'),
+    ('OTH', 'OTHER')
+)
+
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,6 +19,16 @@ class Member(models.Model):
 
 class Question(models.Model):
     question = models.CharField(max_length=500)
+    category = models.CharField(
+        max_length=3,
+        choices=CATEGORIES,
+    )
+    is_anon = models.BooleanField(default=False)
+    points = models.IntegerField(editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
