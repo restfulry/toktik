e051c3d15dfae0e77f4260ee0f8c7c2703a2b71b
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-12 20:52:31 -0500

    -create question form html -update Question model to include absolute url redirect

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 CATEGORIES = (
     ('SCI', 'SCIENCE'),
@@ -44,6 +45,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"question_id": self.id})
 
 
 class Answer(models.Model):
