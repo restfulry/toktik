d0dbfd1bf35bd4a54ed2f066e4a12f0a6007de7a
David Harris <davidharris319@gmail.com>
2021-01-12 19:33:15 -0500

    updated admin to include models

--- main_app/models.py
+++ main_app/models.py
@@ -16,7 +16,7 @@ class Member(models.Model):
     email = models.EmailField(max_length=150)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    points = models.IntegerField(editable=False, default='100')
+    points = models.IntegerField(editable=False, default='1000')
 
     def __str__(self):
         return self.user.username
