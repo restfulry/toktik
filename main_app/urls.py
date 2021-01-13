309a7daf4cac55fc63b0fba41a11f188194b8f44
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-12 20:29:24 -0500

    add question create url to urls.py and base html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('questions/new/', views.QuestionCreate.as_view(), name='questions_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.views.generic.edit import CreateView
 from .forms import SignUpForm
+from .models import Question
 
 
 def home(request):
